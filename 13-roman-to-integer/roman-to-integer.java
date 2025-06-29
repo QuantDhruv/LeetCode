ebd65425fb2e331264e097100e6e552fa4aa6bc4
Dhruv Yadav <155057260+QuantDhruv@users.noreply.github.com>
2025-06-29 22:46:01 +0530

    Time: 5 ms (53.53%) | Memory: 45.2 MB (10.94%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currValue = romanMap.get(s.charAt(i));
+
+            if (currValue < prevValue) {
+                total -= currValue; // Subtract if smaller value before larger one (e.g., IV, IX)
+            } else {
+                total += currValue; // Add otherwise
+            }
+
+            prevValue = currValue;
+        }
+
+        return total;
+    }
+}
